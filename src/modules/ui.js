6fa770e9ace4dd74599ec3741e029f16564bc20c
chaseseidel <chaseseidel16@gmail.com>
2023-07-25 23:43:02 -0700

    Create loadPage function

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,12 +1,12 @@
 import getWeatherData from "./api";
 
-let tempC = true;
-let currentCity = '';
+let currentCity = "London";
 
 export default function buttonEventListener() {
   const button = document.getElementById("submit");
   const input = document.getElementById("search");
-  const tempC = document.getElementById
+  const tempC = document.getElementById("tempC");
+  const tempF = document.getElementById("tempF");
 
   button.addEventListener("click", async (e) => {
     const cityName = document.getElementById("city-name");
@@ -14,7 +14,7 @@ export default function buttonEventListener() {
     try {
       const weather = await getWeatherData(`${input.value}`);
       input.value = "";
-      displayInfo(weather);
+      displayInfoMetric(weather);
       console.log(weather);
       currentCity = weather.location.name;
     } catch (error) {
@@ -28,9 +28,44 @@ export default function buttonEventListener() {
       input.value = "";
     }
   });
+
+  tempC.addEventListener("click", async () => {
+    if (tempC.classList.contains("active")) {
+      return;
+    } else {
+      tempC.className = "temp-button active";
+      tempF.className = "temp-button inactive";
+      try {
+        const weather = await getWeatherData(currentCity);
+        displayInfoMetric(weather);
+      } catch (error) {
+        console.log(`You have an error to look at: ${error}`);
+      }
+    }
+  });
+
+  tempF.addEventListener("click", async () => {
+    if (tempF.classList.contains("active")) {
+      return;
+    } else {
+      tempC.className = "temp-button inactive";
+      tempF.className = "temp-button active";
+      try {
+        const weather = await getWeatherData(currentCity);
+        displayInfoImperial(weather);
+      } catch (error) {
+        console.log(`You have an error to look at: ${error}`);
+      }
+    }
+  });
 }
 
-function displayInfo(city) {
+export async function loadPage() {
+  const weather = await getWeatherData("London");
+  displayInfoMetric(weather);
+}
+
+function displayInfoMetric(city) {
   const cityName = document.getElementById("city-name");
   const currentTemp = document.getElementById("current-temp");
   const conditions = document.getElementById("conditions");
@@ -46,13 +81,42 @@ function displayInfo(city) {
   cityName.textContent = `${city.location.name}, ${city.location.country}`;
   currentTemp.textContent = `${city.current.temp_c} \xB0`;
   conditions.textContent = `${city.current.condition.text}`;
-  updateStatus.textContent = `Updates as of ${convertTimeToTwelveHours(updateTime[1])}`;
+  updateStatus.textContent = `Updates as of ${convertTimeToTwelveHours(
+    updateTime[1]
+  )}`;
   feelsLike.textContent = `Feels Like: ${city.current.feelslike_c} \xB0C`;
   humidity.textContent = `Humidity: ${city.current.humidity}%`;
   uvIndex.textContent = `UV Index: ${city.current.uv}`;
   visibility.textContent = `Visibility: ${city.current.vis_km} KM`;
   wind.textContent = `Wind: ${city.current.wind_kph} KPH ${city.current.wind_dir}`;
   pressure.textContent = `Pressure: ${city.current.pressure_mb} mb`;
+}
+
+function displayInfoImperial(city) {
+  const cityName = document.getElementById("city-name");
+  const currentTemp = document.getElementById("current-temp");
+  const conditions = document.getElementById("conditions");
+  const updateStatus = document.getElementById("update-status");
+  const updateTime = city.current.last_updated.split(" ");
+  const feelsLike = document.getElementById("feelTemp");
+  const humidity = document.getElementById("humidity");
+  const uvIndex = document.getElementById("UV");
+  const visibility = document.getElementById("visibility");
+  const wind = document.getElementById("wind");
+  const pressure = document.getElementById("pressure");
+
+  cityName.textContent = `${city.location.name}, ${city.location.country}`;
+  currentTemp.textContent = `${city.current.temp_f} \xB0`;
+  conditions.textContent = `${city.current.condition.text}`;
+  updateStatus.textContent = `Updates as of ${convertTimeToTwelveHours(
+    updateTime[1]
+  )}`;
+  feelsLike.textContent = `Feels Like: ${city.current.feelslike_f} \xB0F`;
+  humidity.textContent = `Humidity: ${city.current.humidity}%`;
+  uvIndex.textContent = `UV Index: ${city.current.uv}`;
+  visibility.textContent = `Visibility: ${city.current.vis_miles} Miles`;
+  wind.textContent = `Wind: ${city.current.wind_mph} MPH ${city.current.wind_dir}`;
+  pressure.textContent = `Pressure: ${city.current.pressure_in} in`;
 }
 
 function convertTimeToTwelveHours(time) {
@@ -63,8 +127,4 @@ function convertTimeToTwelveHours(time) {
   } else {
     return `${timeArray[0]}:${timeArray[1]} am`;
   }
-}
-
-function changeCelciusToFahrenheit(city) {
-
 }
