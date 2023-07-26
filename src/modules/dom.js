47f8726b3ebd232a2d9a39faa91ae9492497860f
chaseseidel <chaseseidel16@gmail.com>
2023-07-25 23:25:11 -0700

    Create function to build DOM

---
+++ src/modules/dom.js
@@ -0,0 +1,120 @@
+import search from "../img/search.png";
+import weather from "../img/weathericon.png";
+
+export default function buildPage() {
+  const container = document.getElementById("container");
+
+  const mainContent = document.createElement("div");
+  const form = document.createElement("form");
+
+  //-----------------------MAIN CONTENT-----------------------//
+  const cityName = document.createElement("div");
+  const currentInfo = document.createElement("div");
+  const conditions = document.createElement("p");
+  const status = document.createElement("p");
+  const extra = document.createElement("div");
+
+  cityName.setAttribute("id", "city-name");
+  currentInfo.setAttribute("id", "current-info");
+  conditions.setAttribute("id", "conditions");
+  status.setAttribute("id", "update-status");
+  extra.setAttribute("id", "extra-info");
+
+  //-----------------------CURRENT INFO-----------------------//
+  const weatherIcon = document.createElement("img");
+  const currentTemp = document.createElement("p");
+  const tempButtons = document.createElement("div");
+  const tempButtonC = document.createElement("button");
+  const tempButtonF = document.createElement("button");
+
+  weatherIcon.src = weather;
+  weatherIcon.alt = "Current weather icon";
+  weatherIcon.setAttribute("id", "current-icon");
+
+  currentTemp.setAttribute("id", "current-temp");
+
+  tempButtons.setAttribute("id", "temp-buttons");
+  tempButtonC.textContent = "C";
+  tempButtonC.setAttribute("id", "tempC");
+  tempButtonC.className = "temp-button active";
+  tempButtonF.textContent = "F";
+  tempButtonF.setAttribute("id", "tempF");
+  tempButtonF.className = "temp-button inactive";
+
+  tempButtons.appendChild(tempButtonC);
+  tempButtons.appendChild(tempButtonF);
+
+  currentInfo.appendChild(weatherIcon);
+  currentInfo.appendChild(currentTemp);
+  currentInfo.appendChild(tempButtons);
+
+  //-----------------------EXTRA INFO-----------------------//
+  const feelTemp = document.createElement("div");
+  const humidity = document.createElement("div");
+  const uvIndex = document.createElement("div");
+  const visibility = document.createElement("div");
+  const wind = document.createElement("div");
+  const pressure = document.createElement("div");
+
+  feelTemp.textContent = "Feels Like:";
+  feelTemp.classList.add("info");
+  feelTemp.setAttribute("id", "feelTemp");
+
+  humidity.textContent = "Humidity:";
+  humidity.classList.add("info");
+  humidity.setAttribute("id", "humidity");
+
+  uvIndex.textContent = "UV:";
+  uvIndex.classList.add("info");
+  uvIndex.setAttribute("id", "UV");
+
+  visibility.textContent = "Visibility";
+  visibility.classList.add("info");
+  visibility.setAttribute("id", "visibility");
+
+  wind.textContent = "Wind:";
+  wind.classList.add("info");
+  wind.setAttribute("id", "wind");
+
+  pressure.textContent = "Pressure:";
+  pressure.classList.add("info");
+  pressure.setAttribute("id", "pressure");
+
+  extra.appendChild(feelTemp);
+  extra.appendChild(humidity);
+  extra.appendChild(uvIndex);
+  extra.appendChild(visibility);
+  extra.appendChild(wind);
+  extra.appendChild(pressure);
+
+  mainContent.setAttribute("id", "main-info");
+  mainContent.appendChild(cityName);
+  mainContent.appendChild(currentInfo);
+  mainContent.appendChild(conditions);
+  mainContent.appendChild(status);
+  mainContent.appendChild(extra);
+  //-----------------------FORM-----------------------//
+  const input = document.createElement("input");
+  const searchImg = document.createElement("img");
+
+  input.type = "text";
+  input.name = "search";
+  input.placeholder = "Search Location";
+  input.setAttribute("id", "search");
+
+  searchImg.src = search;
+  searchImg.alt = "Search icon";
+  searchImg.setAttribute("id", "submit");
+
+  form.setAttribute("id", "form");
+  form.appendChild(input);
+  form.appendChild(searchImg);
+
+  cityName.textContent = "London, United Kingdom";
+  currentTemp.textContent = "37";
+  conditions.textContent = "Partly Sunny";
+  status.textContent = "Updates as of 01:27";
+
+  container.appendChild(mainContent);
+  container.appendChild(form);
+}
