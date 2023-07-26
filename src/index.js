05018cff70d928be905995d9c0def4ecffa7fc65
chaseseidel <chaseseidel16@gmail.com>
2023-07-26 00:27:33 -0700

    Finish functions for application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("hi");
+import "./css/style.css";
+import buildPage from "./modules/dom";
+import { loadPage } from "./modules/ui";
+import buttonEventListener from "./modules/ui";
+
+buildPage();
+loadPage();
+buttonEventListener();
